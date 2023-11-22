4657ac31a5056583778e06e9631fcf3a7cf4b786
sayehjarollahi <sjarollahi@yahoo.com>
2023-11-22 18:43:40 +0330

    add package shipping test

--- src/test/packageTest.java
+++ src/test/packageTest.java
@@ -8,5 +8,19 @@ public class packageTest {
         Package pkg = new Package(5);
         assertEquals(pkg.getWeight(), 5);
     }
+    @Test
+    public void testSetShipping(){
+        Package pkg = new Package(5);
+        Shipping shipping = new StandardShipping();
+        pkg.setShipping(shipping);
+        double actualPrice = 2.5*5;
+        assertEquals(shipping.getShippingPrice(), actualPrice);
+
+        shipping = new ExpressShipping();
+        pkg.setShipping(shipping);
+        actualPrice = 3.5*5;
+        assertEquals(shipping.getShippingPrice(), actualPrice);
+    }
+
 
 }
